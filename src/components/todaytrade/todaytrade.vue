1df92aea3c9f17841568de390a34cb1d91fa0a92
liuyuan <fenjin1987@gmail.com>
2017-06-23 11:32:07 +0800

    离开路由时结束自动刷新，请求失败时，结束自动刷新并提示重试

--- src/components/todaytrade/todaytrade.vue
+++ src/components/todaytrade/todaytrade.vue
@@ -20,8 +20,8 @@
           row-class-name="el-table__row_fix"
           class="trade-table">
           <el-table-column
-            prop="shopname"
             label="店铺名称/ID">
+            <template scope="scope">{{ shop.shopname }}</template>
           </el-table-column>
           <el-table-column
             prop="opuid"
@@ -67,23 +67,31 @@
 <script>
   import axios from 'axios'
   import config from 'config'
-  // export '../../mock/data.json'
   export default {
-    props: ['shop'],
+    props: {
+      shop: {
+        type: Object
+      }
+    },
     data() {
       return {
         trades: [],
-        loading: false
+        loading: false,
+        timeId: null
       }
     },
     created() {
       this.fetchData()
     },
+    beforeRouteLeave (to, from, next) {
+      clearTimeout(this.timeId)
+      next()
+    },
     methods: {
       fetchData() {
         this.loading = true
         this.trades = []
-        axios.get(`${config.o2host}/trade/v1/tradelist?busicd=000000,700000,800101,800107,800108,800201,800207,800208,800401,800407,800408,800501,800507,800508,800601,800607,800608&start=1&len=10&respcd=0000&fix=1&stat=0`)
+        axios.get(`${config.o2host}/trade/v1/tradelist?busicd=000000,700000,800101,800107,800108,800201,800207,800208,800401,800407,800408,800501,800507,800508,800601,800607,800608&start=1&len=10&respcd=0000&fix=1&stat=0&format=cors`)
         .then((res) => {
           let data = res.data
           if (data.respcd === config.code.OK) {
@@ -101,16 +109,18 @@
                 if (tradeskey[variable] === 'opuid') {
                   tradesObject['opuid'] = '000' + tradesObject['opuid']
                 }
-                tradesObject['shopname'] = this.shop.shopname
               }
               this.trades.push(tradesObject)
             }
             this.loading = false
           } else {
-            this.$message.error(data.respmsg)
+            this.loading = false
+            this.trades = []
+            clearTimeout(this.timeId)
+            this.$message.error(data.resperr + '，请刷新页面重试')
           }
         })
-        setTimeout(this.fetchData, 5000)
+        this.timeId = setTimeout(this.fetchData, 5000)
       }
     }
   }
@@ -123,5 +133,17 @@
   .trade-table {
     width:100%;
     min-height:661px;
+  }
+  .view-more {
+    text-align: right;
+    line-height: 50px;
+    padding-right: 30px;
+    a {
+      color: #FE9B20;
+      font-size: 17px;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
   }
 </style>
