238563b4b2e5face5d64cf0f0af54b103ef85ac9
guoxin <guoxin@qfpay.com>
2017-12-05 18:06:55 +0800

    eslint多变量定义

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,9 +8,6 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
-var env = process.env.NODE_ENV === 'test' ? require('../config/test.env') : config.build.env
-
-
 var webpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({
@@ -27,7 +24,7 @@ var webpackConfig = merge(baseWebpackConfig, {
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
-      'process.env': env
+      'process.env': config.build.env
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
