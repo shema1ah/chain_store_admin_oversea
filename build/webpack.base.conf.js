5586fd455e32bb01069eac10ff02040328b8f42b
左楠 <zuonan@meituan.com>
2019-03-13 16:15:08 +0800

    sovlve the pack

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -37,7 +37,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('node_modules/qfpay-element-ui'), resolve('node_modules/qfpay-element-ui/src')]
+        include: [resolve('src'), resolve('node_modules/qfpay-element-ui'), resolve('node_modules/qfpay-element-ui/src'),resolve('node_modules/echarts/lib')]
       },
       {
         test: /\.(js|vue)$/,
