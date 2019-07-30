dfa9171d2e56020d68cc8bf6861c664b16c061b6
yangqingkun <yangqingkun@qfpay.com>
2019-07-30 12:17:31 +0800

    修改动态host

--- src/App.vue
+++ src/App.vue
@@ -16,30 +16,31 @@
       setConfig() {
         let [uri, u] = [];
         // 本地调试
-        if(process.env.NODE_ENV === 'development') {
+        if (process.env.NODE_ENV === 'development') {
            u = 'qa.qfpay.net';
            uri = {
-             host: `https://sh.${u}`,
+
+            // host: `https://sh.${u}`,
+            host: `https://sh-kh.qfapi.com`,
             //  host: `http://172.100.108.211:9099`,
-             oHost: `https://o.${u}`,
-             // oHost: `http://172.100.108.190:7200`,
-            //  payHost: 'api'
-             payHost: `https://openapi.${u}`,
+
+            // oHost: `https://o.${u}`,
+            oHost: `https://o-kh.qfapi.com`,
+            // oHost: `http://172.100.108.190:7200`,
+
+            // payHost: `https://openapi.${u}`,
+            payHost: `https://openapi-kh.qfapi.com`,
+
            };
 
-        } else if(location.hostname.includes('kh.qfapi')) {
-          // 柬埔寨环境
-          uri = {
-            host: `https://sh-kh.qfapi.com`,
-            oHost: `https://o-kh.qfapi.com`,
-            payHost: `https://openapi-kh.qfapi.com`,
-          };
-        }else {
-          uri = Store.get('hosts') || {
-            host: `https://sh.qfpay.com`,
-            oHost: `https://o.qfpay.com`,
-            payHost: `https://openapi.qfpay.com`,
-          };
+        } else {
+          let u = location.hostname;
+          let pro = location.protocol;
+          uir = {
+            host: `${pro}//${u}`,
+            oHost: `${pro}//${u.replace('sh', 'o')}`,
+            payHost: `${pro}//${u.replace('sh', 'openapi')}`
+          }
         }
         Object.assign(config, uri);
       }
