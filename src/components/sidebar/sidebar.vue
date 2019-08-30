b8849d7106f2245936416ec191b8fc2507665b6a
yangqingkun <yangqingkun@qfpay.com>
2019-08-30 15:00:19 +0800

    删除多余获取sidebar的methods

--- src/components/sidebar/sidebar.vue
+++ src/components/sidebar/sidebar.vue
@@ -61,22 +61,22 @@
 
     created() {
       this.getNav();
-      if (this.role.diancan && !this.role.isCashier) {
-        this.navs.splice(-1, 0, {
-          val: this.$t('diancan.Qr.smart'), // 智慧餐厅
-          subnav: 'merchant',
-          sub: [{
-            val: this.$t('diancan.Qr.qr'), // 桌台二维码
-            pathname: 'qrcode'
-          }, {
-            val: this.$t('diancan.Manage.manage'), // 桌台二维码
-            pathname: 'merchandise'
-          }, {
-            val: this.$t('diancan.Rank.index'), // 商品排序
-            pathname: 'rank'
-          }]
-        })
-      }
+      // if (this.role.diancan && !this.role.isCashier) {
+      //   this.navs.splice(-1, 0, {
+      //     val: this.$t('diancan.Qr.smart'), // 智慧餐厅
+      //     subnav: 'merchant',
+      //     sub: [{
+      //       val: this.$t('diancan.Qr.qr'), // 桌台二维码
+      //       pathname: 'qrcode'
+      //     }, {
+      //       val: this.$t('diancan.Manage.manage'), // 桌台二维码
+      //       pathname: 'merchandise'
+      //     }, {
+      //       val: this.$t('diancan.Rank.index'), // 商品排序
+      //       pathname: 'rank'
+      //     }]
+      //   })
+      // }
     },
     computed: {
       preAuthA() {
