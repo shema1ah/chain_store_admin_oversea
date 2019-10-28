1ffaf1e510bc896a0498cea932ad4c75545ee3c3
hujiajia <hujiajia@qfpay.com>
2019-10-28 14:33:36 +0800

    清算管理快速切换菜单时 控制体台包dom找不到的

--- src/components/settlement/settlement.vue
+++ src/components/settlement/settlement.vue
@@ -306,7 +306,7 @@
         if (current) {
           this.currentPage = current;
         }
-    
+
         this.getSettleData();
       },
 
@@ -332,14 +332,15 @@
           let el = document.createElement("div");
           el.innerHTML = this.$t('settlement.tip.m1');
           el.setAttribute('id', 'toolShow');
-          toolShow.appendChild(el);
-
-          toolShow.addEventListener('mouseenter', function () {
-            el.style.display = 'block';
-          }, false);
-          toolShow.addEventListener('mouseleave', function () {
-            el.style.display = 'none';
-          }, false);
+          if (toolShow) { // 由于是定时触发 当快速切换菜单后当前组件内容被销毁 toolShow 为null
+            toolShow.appendChild(el);
+            toolShow.addEventListener('mouseenter', function () {
+              el.style.display = 'block';
+            }, false);
+            toolShow.addEventListener('mouseleave', function () {
+              el.style.display = 'none';
+            }, false);
+          }
         }
       }
     }
